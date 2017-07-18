ff609b207f43413d11f683e71308532ac8a74c23
Shane Zackery <shane@Shane-Zackerys-Macbook-Pro.local>
2017-07-17 17:52:08 -0700

    implement domain_hrefs method

--- document.rb
+++ document.rb
@@ -7,11 +7,15 @@ class Document
 		@content = content
 	end
 
-	def hrefs
-		@hrefs ||= links.map { |link| link['href'] }.uniq
+	def domain_hrefs
+		@domain_hrefs ||= hrefs.grep (/^\/wiki\//)
 	end
 
 	private
+
+	def hrefs
+		@hrefs ||= links.map { |link| link['href'] }.uniq
+	end
 
 	def parsed
 		@parsed ||= Nokogiri::HTML(@content)
