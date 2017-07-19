7d638572c4ff98592dd6ec4e07e082970ede5fa3
shanemachine92 <shane@entelo.com>
2017-07-19 11:48:14 -0700

    implement base class for url collection (#2)
    can now crawl with using either breadth first or depth first approach

---
+++ base_url_collection.rb
@@ -0,0 +1,17 @@
+class UrlBaseCollection
+	def next_url
+		raise NotImplementedError
+	end
+
+	def add_url(url)
+		raise NotImplementedError
+	end
+
+	def size
+		raise NotImplementedError
+	end
+
+	def empty?
+		raise NotImplementedError
+	end
+end
