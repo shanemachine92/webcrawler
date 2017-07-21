a5425133fe0eef8007cfe3053f139f199240c7ad
Shane Zackery <shane@Shane-Zackerys-Macbook-Pro.local>
2017-07-21 13:16:19 -0700

    test document class

--- spec/breadth_first_url_collection_spec.rb
+++ spec/breadth_first_url_collection_spec.rb
@@ -3,7 +3,7 @@ require_relative '../script'
 RSpec.describe BreadthFirstUrlCollection do
 	subject(:url_collection) { BreadthFirstUrlCollection.new }
 
-	it "lets you add a url and store it as next url" do
+	it "lets you add a url and store it as the next link" do
 		url_collection.add_url("/first")
 		expect(url_collection.size).to eq(1)
 		expect(url_collection.next_url).to eq("http://dragonage.wikia.com/first")
