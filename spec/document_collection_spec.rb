22dcb6b9693e6e037e4b86df97f4951826e00a1b
Shane Zackery <shane@Shane-Zackerys-Macbook-Pro.local>
2017-07-21 11:50:34 -0700

    test url collector

--- spec/document_collection_spec.rb
+++ spec/document_collection_spec.rb
@@ -3,7 +3,7 @@ require_relative '../script'
 RSpec.describe DocumentCollection do
 	subject(:document_collection) { DocumentCollection.new }
 
-	it "let's me add a document" do
+	it "lets you add a document" do
 		document_collection.add_document(Document.new('www.something.com', 'stuff'))
 		expect(document_collection.size).to eq(1)
 	end
