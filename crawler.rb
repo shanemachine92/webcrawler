0a8626376a25d36cde3139c4bd6ac105dcd48927
Shane Zackery <shane@Shane-Zackerys-Macbook-Pro.local>
2017-08-17 14:31:39 -0700

    create db write methods

--- crawler.rb
+++ crawler.rb
@@ -1,57 +1,71 @@
+require 'pry'
+require 'set'
+require 'JSON'
 require 'nokogiri'
 require 'open-uri'
-require 'JSON'
-require 'pry'
 require_relative './document'
-require_relative './document_collection'
 require_relative './url_fetcher'
-require 'set'
-
+require_relative './document_collection'
 
 class Crawler
-	attr_accessor :url, :href
-	def initialize(document_collection, url_collection)
-		@document_collection = document_collection
-		@url_collection = url_collection
-		@set = Set.new
-	end
+  attr_accessor :url, :href, :db
+  def initialize(document_collection, url_collection, db)
+    @document_collection = document_collection
+    @url_collection = url_collection
+    @set = Set.new
+    @database = db 
+  end
 
-	def done_crawling?
-		@url_collection.empty?
-	end
+  def done_crawling?
+    @url_collection.empty?
+  end
 
-	def crawl_next_url
-		url = @url_collection.next_url
-		content = UrlFetcher.fetch(url)
-		document = Document.new(url, content)
-		@document_collection.add_document(document)
-		puts document.domain_hrefs.take(5)
-		document.domain_hrefs.each do |href|
-			next if already_crawled?(href)
-			@url_collection.add_url(href)
-			track_url(href)
-		end
-		puts "set size: #{@set.size}"
-		puts "url collection size: #{@url_collection.size}"
-	end
+  def crawl_next_url
+    url = @url_collection.next_url
+    state = "uncrawled"
+    content = UrlFetcher.fetch(url)
+    document = Document.new(url, content)
+    @document_collection.add_document(document)
+    puts document.domain_hrefs.take(5)
+    document.domain_hrefs.each do |href|
+      next if already_crawled?(href)
+      @url_collection.add_url(href)
+      track_url(href)
+      write_to_db
+      update_state
+    end
+  end
 
-	def already_crawled?(url)
-		@set.include?(url)
-	end
+  def update_state
+    state = "crawled"
+  end
 
-	def track_url(url)
-		@set.add(url)
-	end
+  def already_crawled?(url)
+    @set.include?(url)
+  end
 
-	def run
-		begin
-			if done_crawling?
-				puts "Crawling complete!"
-				exit 0
-			end
-			crawl_next_url
-		rescue OpenURI::HTTPError=>e
-			puts "Error: #{e}"
-		end
-	end
+  def track_url(url)
+    @set.add(url)
+  end
+
+  def write_to_db(url, state)
+    @database.execute("INSERT INTO URLS_to_crawl (url, state) 
+                VALUES (?, ?)", [url, state])
+
+    @database.execute( "select * from URLS_to_crawl" ) do |row|
+      p row
+    end
+  end
+
+  def run
+    begin
+      if done_crawling?
+        puts "Crawling complete!"
+        exit 0
+      end
+      crawl_next_url
+    rescue OpenURI::HTTPError=>e
+      puts "Error: #{e}"
+    end
+  end
 end
--- script.rb
+++ script.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'sqlite3'
 require_relative './crawler'
 require_relative './document_collection'
 require_relative './breadth_first_url_collection'
@@ -7,7 +8,7 @@ require_relative './depth_first_url_collection'
 BASE_URL = 'http://dragonage.wikia.com'
 
 document_collection = DocumentCollection.new
-url_collection = DepthFirstUrlCollection.new
+url_collection = BreadthFirstUrlCollection.new
 url_collection.add_url('')
 crawler = Crawler.new(document_collection, url_collection)
 
